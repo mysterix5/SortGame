6336f4e8b10a469f374c921a4446375426292a1b
lukas <lukasduechting@mailbox.org>
2022-08-23 13:48:22 +0200

    no crossorigin

--- src/main/java/io/github/mysterix5/sortgame/game/service/SortGameController.java
+++ src/main/java/io/github/mysterix5/sortgame/game/service/SortGameController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 
-@CrossOrigin
+//@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/game")
